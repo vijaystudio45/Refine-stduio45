15c19131bb7f659ae2096c4440e28262987cff13
vijaystudio45 <vijaya.logiclab@gmail.com>
2023-11-15 18:43:14 +0530

    update

--- backendcode/Blogpost/serializers.py
+++ backendcode/Blogpost/serializers.py
@@ -3,9 +3,15 @@ from rest_framework import serializers
 from .models import  Category,Blog
 
 class CategorySerializer(serializers.ModelSerializer):
+    title = serializers.SerializerMethodField()
+
     class Meta:
         model = Category
         fields = '__all__'
+
+    def get_title(self, obj):
+        return obj.category
+
         
         
 class BlogSerializer(serializers.ModelSerializer):
